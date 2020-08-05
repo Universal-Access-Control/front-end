c8e0d3a45c7efc0cad703217c1bfe114d4dc6ceb
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-05 05:15:59 +0430

    Install and config `Tailwindcss`

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        layout: '4rem 1fr',
+      },
+      gridTemplateColumns: {
+        layout: '4rem 1fr',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
