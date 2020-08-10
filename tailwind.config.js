d3754883aa4b68f521f653e328959c50b59a4128
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-10 17:51:30 +0430

    Update theme's colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,91 @@
 module.exports = {
   theme: {
+    colors: {
+      // INFO COLORS
+      infoLight: '#daf0ff',
+      info: '#44c3fd',
+      infoDark: '#2d5d76',
+
+      // WARNING COLORS
+      warningLight: '#feefd6',
+      warning: '#ebc05d',
+      warningDark: '#6f5c31',
+
+      // SUCCESS COLORS
+      successLight: '#daf4db',
+      success: '#5ed173',
+      successDark: '#33633a',
+
+      // DANGER COLORS
+      dangerLight: '#ffd3d7',
+      danger: '#eb3d67',
+      dangerDark: '#702634',
+
+      // BLACK COLORS
+      black: {
+        100: '#cfd0d8',
+        200: '#9fa1b0',
+        300: '#6e7389',
+        400: '#3e4461',
+        500: '#0e153a',
+        600: '#0b112e',
+        700: '#080d23',
+        800: '#060817',
+        900: '#03040c',
+      },
+
+      // BLUE COLORS
+      blue: {
+        100: '#d8defc',
+        200: '#b1bdf9',
+        300: '#8b9cf7',
+        400: '#647bf4',
+        500: '#3d5af1',
+        600: '#3148c1',
+        700: '#253691',
+        800: '#182460',
+        900: '#0c1230',
+      },
+
+      // TEAL COLORS
+      teal: {
+        100: '#d3f6fc',
+        200: '#a7edf8',
+        300: '#7ae3f5',
+        400: '#4edaf1',
+        500: '#22d1ee',
+        600: '#1ba7be',
+        700: '#147d8f',
+        800: '#0e545f',
+        900: '#072a30',
+      },
+
+      // RED COLORS
+      red: {
+        100: '#ffd6dc',
+        200: '#ffacb9',
+        300: '#ff8395',
+        400: '#ff5972',
+        500: '#ff304f',
+        600: '#cc263f',
+        700: '#991d2f',
+        800: '#661320',
+        900: '#330a10',
+      },
+
+      // GRAY COLORS
+      gray: {
+        100: '#f9fdfd',
+        200: '#f3fafb',
+        300: '#eef8f9',
+        400: '#e8f5f7',
+        500: '#e2f3f5',
+        600: '#b5c2c4',
+        700: '#889293',
+        800: '#5a6162',
+        900: '#2d3131',
+      },
+    },
     extend: {
       gridTemplateRows: {
         layout: '4rem 1fr',
