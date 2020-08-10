e5b5ccf6156c9f8a66a2b03e154788890cba9fa4
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-10 18:39:23 +0430

    Install and config graphql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Apollo
+import { ApolloProvider } from '@apollo/client';
+
+// Main core
 import App from './App';
+import apolloClient from './services/apollo';
+
+// CSS styles
 import 'tailwindcss/dist/base.min.css';
 
+// ==================================================
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
