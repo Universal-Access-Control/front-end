16a93af312778386572f45d0b9b49765b6ffac91
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-14 18:22:45 +0430

    🔥 Remove tailwind's custom color palette  💄 Update components with new color palette

--- src/components/dashboard-layout/sidebar.js
+++ src/components/dashboard-layout/sidebar.js
@@ -4,6 +4,9 @@ import React from 'react';
 // Tailwindcss
 import tw, { styled } from 'twin.macro';
 
+// Styled-Tools
+import { ifProp } from 'styled-tools';
+
 // React-icon
 import { FiGrid, FiSettings, FiUser } from 'react-icons/fi';
 import { GoPlus } from 'react-icons/go';
@@ -11,22 +14,30 @@ import { GoPlus } from 'react-icons/go';
 // ===========================================
 const SidebarContainer = tw.aside`
   fixed top-0 left-0 w-48 h-full
-  bg-black-500 text-gray-100
+  bg-gray-900 text-gray-100
 `;
-const ItemsHeader = tw.h5`font-semibold text-lg mx-4 mt-8 mb-4 text-black-300 uppercase`;
+const ItemsHeader = tw.h5`font-medium text-lg mx-4 mt-8 mb-4 text-gray-600 uppercase`;
 const Item = styled.li`
-  ${tw`flex items-center p-4 pl-8 text-sm cursor-pointer hover:bg-black-400`}
-  ${({ active }) => active && tw`bg-red-500! border-teal-500`}
+  ${tw`flex items-center p-4 pl-8 text-sm text-gray-300 cursor-pointer hover:bg-gray-800`}
+  ${tw`transition-colors duration-300 ease-out`}
+  ${ifProp('active', tw`text-gray-100 bg-blue-600 shadow hover:bg-blue-500`, null)}
   svg {
     ${tw`mr-4 text-xl`}
   }
 `;
-const Brand = tw.h1`p-4 my-4 font-itim text-2xl text-red-500 font-bold whitespace-no-wrap`;
+const Brand = styled.h1`
+  ${tw`p-4 my-4 text-xl font-bold whitespace-no-wrap`}
+  span {
+    ${tw`text-blue-400`}
+  }
+`;
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
-      <Brand>Access Control</Brand>
+      <Brand>
+        Access <span>Control</span>
+      </Brand>
       <ItemsHeader>Manage</ItemsHeader>
       <ul>
         <Item active>
