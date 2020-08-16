da4db84cb9c2e5f0bd4a80f1072fb95a3f426873
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-08-16 16:26:18 +0430

    💄 Show loading spinner when page is loading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import React, { lazy, Suspense } from 'react';
 // React-Router
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+// Tailwind
+import tw, { theme, styled } from 'twin.macro';
+
+// React-Spinkit
+import Spinner from 'react-spinkit';
+
 // Main core
 import DashboardLayout from 'components/dashboard-layout';
 
@@ -11,12 +17,19 @@ const Dashboard = lazy(() => import('pages/dashboard'));
 const Devices = lazy(() => import('pages/devices'));
 
 // ==================================
+const PageLoading = styled(Spinner)`
+  ${tw`flex justify-center p-4 mt-10`}
+  & > div {
+    ${tw`w-10 h-10`}
+  }
+`;
+
 const AppRoute = ({ Component, Layout, ...routerProps }) => (
   <Route
     {...routerProps}
     render={(props) => (
       <Layout>
-        <Suspense fallback={<div>loading...</div>}>
+        <Suspense fallback={<PageLoading name="pulse" color={theme`colors.gray.600`} />}>
           <Component {...props} />
         </Suspense>
       </Layout>
